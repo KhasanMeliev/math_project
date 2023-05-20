5dc006a7db0a97d6ebf220e793ba81aa2e29dd0b
Khasan Meliev <khasanmeliev2@gmail.com>
2023-05-20 22:02:49 +0500

    math

---
+++ src/quizData.js
@@ -0,0 +1,49 @@
+const quizData = [
+  {
+    question: "Tenglamani yeching: 2𝑥2 = 32",
+    answers: ["𝑥1 = −8, 𝑥2 = 8", " 𝑥1 = −6, 𝑥2 = 6", "𝑥1 = −4, 𝑥2 = 4", "x<0"],
+    correctAnswer: "𝑥1 = −4, 𝑥2 = 4",
+    ball: "3.5",
+  },
+  {
+    question: "Tenglamani yeching: x2-8x=-16",
+    answers: ["x=4", "x1,2=+-4", "x=0", "x=-4"],
+    correctAnswer: "x=4",
+    ball: "6",
+  },
+  {
+    question:
+      "Ildizlari −0,5 va 2 ga teng bo‘lgan kvadrat tenglamani ko‘rsating.",
+    answers: [
+      "−2𝑥2 − 3𝑥 + 2 = 0 ",
+      "−2𝑥2 + 3𝑥 − 2 = 0",
+      "−2𝑥2 + 3𝑥 + 2 = 0",
+      "2𝑥2 + 3𝑥 + 2 = 0",
+    ],
+    correctAnswer: "−2𝑥2 + 3𝑥 + 2 = 0",
+    ball: "2",
+  },
+  {
+    question:
+      "Bikvadrat tenglamaning ildizlari yig‘indisini toping: 𝑥4 + 𝑥2 − 13 = 0",
+    answers: ["-2", "1.5", "0", "2.5"],
+    correctAnswer: "-2",
+    ball: "3.5",
+  },
+  {
+    question:
+      "Agar 𝑥1 va 𝑥2 sonlari −2𝑥2 + 5𝑥 + 1 = 0 tenglamaning ildizlari bo‘lsa, 𝑥2/𝑥1+𝑥1/𝑥2ning qiymatini toping:",
+    answers: ["6", "0", "4", "3"],
+    correctAnswer: "6",
+    ball: "3.5",
+  },
+  {
+    question:
+      "Tenglamani eng katta va eng kichik ildizlarining ayirmasini toping: (𝑥2 − 4𝑥)2 − 2(𝑥 − 2)2 − 6 = 1",
+    answers: ["x=4", "x1,2=+-4", "x=0", "x=-4"],
+    correctAnswer: "x=4",
+    ball: "6",
+  },
+];
+
+export default quizData;
