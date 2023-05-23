486d0f42249229d8bceb78b2fd818ee034c45aed
Khasan Meliev <khasanmeliev2@gmail.com>
2023-05-23 20:55:04 +0500

    added form validation

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,56 +1,57 @@
 import { Button, Select, TextInput } from "@mantine/core";
-import { useForm } from "@mantine/form";
 import "./FormStyle.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import swal from "sweetalert";
 
 function Form() {
-  const form = useForm({
-    initialValues: { name: "", email: "", class: "" },
-    validate: {
-      name: (value) =>
-        value.length < 2 ? "Name must have at least 2 letters" : null,
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
-      class: (value) => (value === "" ? "Choose your class" : null),
-    },
-  });
+  const navigate = useNavigate();
+
+  const initialValues = {
+    name: "",
+    email: "",
+  };
+  const [details, setDetails] = useState(initialValues);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (details.name === "") {
+      swal("Error!", "Name isn't blank", "error");
+      return false;
+    } else if (details.email === "") {
+      swal("Error!", "Email isn't blank or less than 6 characters", "error");
+      return false;
+    } else {
+      swal("Success!", "Successfully Logined", "success");
+      return navigate("/quizapp");
+    }
+  };
   return (
     <div className="formWrapper">
       <h2>Saytga kirish</h2>
-      <form onSubmit={form.onSubmit(console.log())}>
+      <form onSubmit={handleSubmit} autocomplete="off">
         <TextInput
           label="Name"
           placeholder="Name"
-          {...form.getInputProps("name")}
+          autoComplete="off"
+          value={details.name}
+          onChange={(e) => {
+            setDetails({ ...details, name: e.target.value });
+          }}
         />
         <TextInput
           label="Email"
           placeholder="Email"
-          {...form.getInputProps("email")}
+          value={details.email}
+          onChange={(e) => {
+            setDetails({ ...details, email: e.target.value });
+          }}
         />
-        <Select
-          label="Sinfingizni tanlang"
-          placeholder="Sinf"
-          searchable
-          data={[
-            { value: "5", label: "5" },
-            { value: "6", label: "6" },
-            { value: "7", label: "7" },
-            { value: "8", label: "8" },
-            { value: "9", label: "9" },
-            { value: "10", label: "10" },
-            { value: "11", label: "11" },
-          ]}
-          {...form.getInputProps("class")}
-        />
+
         <Button type="submit" mt="sm">
           Submit
         </Button>
       </form>
-      <Button style={{ clor: "white", marginTop: "20px" }}>
-        <Link to="/quizapp" style={{ textDecoration: "none", color: "white" }}>
-          Quiz App ga o'tish
-        </Link>
-      </Button>
     </div>
   );
 }
