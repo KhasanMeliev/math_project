d10875d656673cbf088a7cfc598b377cab974710
melievkh <khushnudmeliev@gmail.com>
2023-06-11 22:56:57 +0500

    new function added

--- src/pages/FileQuiz/FileQuiz.jsx
+++ src/pages/FileQuiz/FileQuiz.jsx
@@ -4,8 +4,13 @@ import { Select, TextInput } from "@mantine/core";
 import { useState } from "react";
 
 const FileQuiz = () => {
-  const [sinf, setSinf] = useState()
-  const [javob, setJavob] = useState("")
+  const [check, setCheck] = useState({
+    sinf: "",
+    javob: "",
+  });
+  const [correct, setCorrect] = useState()
+  const [wrong, setWrong] = useState()
+  const [score, setScore] = useState()
 
   let answers = {
     5: "DCADACCABDBDCDDABAACBDABCABCDB", //1
@@ -17,17 +22,54 @@ const FileQuiz = () => {
     11: "CBAABBCADCCDBDCCBABD", //2
   };
 
-  function tekshirish(sinf, javob) {
-    // let count = 0;
-    // let clas = answers[sinf];
-    // for (let i = 0; i < javob.length; i++) {
-    //   if (clas[i] === javob[i]) {
-    //     count += 1;
-    //   }
-    // }
-    // console.log(count);
-    console.log(setJavob(javob))
+  function tekshirish(e) {
+    e.preventDefault()
+    let count = 0;
+    let wrongAns = 0;
+    let score = 0
+    let clas = answers[check.sinf];
+
+    if (clas === '5' || clas === '6' && check.javob.length !== 30) {
+        console.log('xato')
+    }
+    else if (clas === '7' || clas === '8' || clas === '9' || clas === '10' || clas === '11') {
+      if (check.javob.length !== 20) {
+        console.log('xato')
+      }
+    }
+    else {
+      console.log('true')
+    }
+    for (let i = 0; i < check.javob.length; i++) {
+      if (clas[i] === check.javob[i]) {
+        count += 1;
+      }
+    }
+    wrongAns = check.javob.length - count;
+    score = (count / check.javob.length) * 100
+    setCorrect(count)
+    setWrong(wrongAns)
+    setScore(score)
   }
+
+  const onChange = (e) => {
+    setCheck({ ...check, [e.target.name]: e.target.value });
+  };
+
+  const inputs = [
+    {
+      id: 1,
+      name: "sinf",
+      type: "number",
+      placeholder: "Sinfingizni yozing",
+    },
+    {
+      id: 2,
+      name: "javob",
+      type: "text",
+      placeholder: "Javobingizni yozing",
+    }
+  ];
 
   return (
     <div className="wrapper">
@@ -41,29 +83,33 @@ const FileQuiz = () => {
         <div>11 - sinf Algebra</div>
       </div>
       <div className="javob">
-        <Select
-          label="Nechanchi sinfni testi?"
-          placeholder="Sinf"
-          data={[
-            { value: "5", label: "5" },
-            { value: "6", label: "6" },
-            { value: "7", label: "7" },
-            { value: "8", label: "8" },
-            { value: "9", label: "9" },
-            { value: "10", label: "10" },
-            { value: "11", label: "11" },
-          ]}
-          className="input"
-        />
-        <TextInput
-          label="Javobni yozing..."
-          placeholder="ABCD...."
-          className="input"
-          name="javob"
-          value={javob} 
-          onChange={e => setJavob(e.target.value)}
-        />
-        <button onClick={tekshirish}>Submit</button>
+        {inputs.map((input) => (
+          <>
+            <TextInput
+              key={input.id}
+              {...input}
+              value={check[input.name]}
+              onChange={onChange}
+            />
+            <i>{input.errorMessage}</i>
+          </>
+        ))}
+        <button onClick={tekshirish} >Submit</button>
+      </div>
+      <div className="result">
+        <h3>Natija</h3>
+        <p>
+          Jami savollar: <span>{check.sinf == '5' || check.sinf == '6' ? 30 : 20}</span>
+        </p>
+        <p>
+          To'g'ri javoblar: <span>{correct}</span>
+        </p>
+        <p>
+          No'to'g'ri javoblar: <span>{wrong}</span>
+        </p>
+        <p>
+          Foiz ko'rsatkich: <span>{Math.floor(score)}</span>%
+        </p>
       </div>
     </div>
   );
